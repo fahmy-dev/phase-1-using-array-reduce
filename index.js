77de8e49aefedca3fe6375199c908707dfb8ce54
fahmy-dev <faahmy@proton.me>
2024-12-06 12:10:57 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+let totalBatteries = batteryBatches.reduce(function (batteries, batch) {
+    return batch + batteries; 
+}, 0);
+
